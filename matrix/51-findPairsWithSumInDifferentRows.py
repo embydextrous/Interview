9c9767dbebd33c6944c60672acb55dc5ffa9cc71
embydextrous <arjitagarwal099@email>
2022-04-15 19:32:33 +0530

    Revise 51

--- matrix/51-findPairsWithSumInDifferentRows.py
+++ matrix/51-findPairsWithSumInDifferentRows.py
@@ -1,13 +1,14 @@
 def findPairs(M, x):
-    d = {}
-    for i in range(len(M)):
-        for j in range(len(M[0])):
-            d[M[i][j]] = (i, j)
-    for i in range(len(M)):
-        for j in range(len(M[0])):
-            if x - M[i][j] in d and i < d[x - M[i][j]][0]:
+    s = set(M[0])
+    R, C = len(M), len(M[0])
+    for i in range(1, R):
+        rowSet = set()
+        for j in range(C):
+            if x - M[i][j] in s:
                 print(M[i][j], x - M[i][j])
-    
+            rowSet.add(M[i][j])
+        s = s.union(rowSet)
+
 sum = 11
 mat = [[1, 3, 2, 4],
        [5, 8, 7, 6],
